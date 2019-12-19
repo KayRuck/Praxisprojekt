c3b4c855d7f89e1a19482462aea9db67bcc7e4b4
KayRuck <kay.nicole.ruck@web.de>
2019-12-19 20:46:40 +0100

    Rearranged Fragment correct Typos

--- Frontend/app/src/main/java/com/example/praxisprojekt/fragmente/CourseDetailFragment.kt
+++ Frontend/app/src/main/java/com/example/praxisprojekt/fragmente/CourseDetailFragment.kt
@@ -1,43 +1,42 @@
 package com.example.praxisprojekt.fragmente
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import androidx.preference.PreferenceManager
 import com.example.praxisprojekt.InReturns
 import com.example.praxisprojekt.MainActivity
 import com.example.praxisprojekt.Mods
 import com.example.praxisprojekt.R
 import com.example.praxisprojekt.retrofit.RetroCourse
-import com.example.praxisprojekt.viewModels.CourseDetailViewModel
 import kotlinx.android.synthetic.main.course_detail_fragment.view.*
 
-class CourseDetailFragment(private val retroCourse: RetroCourse, private val ownCourse: Boolean = false) :
+class CourseDetailFragment(
+    private val retroCourse: RetroCourse,
+    private val ownCourse: Boolean = false
+) :
     Fragment() {
 
-    companion object {
-        val TAG = CourseDetailFragment::class.java.canonicalName.toString()
-    }
-
-    private lateinit var viewModel: CourseDetailViewModel
+    private lateinit var rootView: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView: View = inflater.inflate(R.layout.course_detail_fragment, container, false)
+        rootView = inflater.inflate(R.layout.course_detail_fragment, container, false)
 
-        Log.d(TAG, "Fragment loaded")
+        setContent()
+        return rootView
+    }
+
+    private fun setContent() {
 
         val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
         val userID = sharedPref.getInt(MainActivity.USER_ID, -1)
         val userName = sharedPref.getString(MainActivity.USER_NAME, "User No.1")
-
 
         rootView.detailCourseTitleTV.text = retroCourse.title
         rootView.detailCourseDescriptionTV.text = retroCourse.description
@@ -56,13 +55,14 @@ class CourseDetailFragment(private val retroCourse: RetroCourse, private val own
             5 -> rootView.detailCourseModuleTV.text = Mods.BWL1INFMOD.title
         }
 
-        if (retroCourse.privateUsage) rootView.detailCoursePrivateTV.text = R.string.tutoring_p.toString()
+        if (retroCourse.privateUsage) rootView.detailCoursePrivateTV.text =
+            R.string.tutoring_p.toString()
         else rootView.detailCoursePrivateTV.text = R.string.tutoring_g.toString()
 
 
         if (retroCourse.fk_creator == userID) rootView.detailCourseCreator.text = userName
         else {
-            val userWithString =  R.string.userWithId.toString() + retroCourse.fk_creator
+            val userWithString = R.string.userWithId.toString() + retroCourse.fk_creator
             rootView.detailCourseCreator.text = userWithString
         }
 
@@ -75,24 +75,16 @@ class CourseDetailFragment(private val retroCourse: RetroCourse, private val own
             rootView.detailContactButton.setOnClickListener {
 
                 val builder = AlertDialog.Builder(context!!, R.style.CustomAlertDialog)
-                builder.setMessage("Kontaktmöglichkeit auswählen")
+                builder.setMessage("Art des Kontakts auswählen")
                 builder.setCancelable(true)
 
                 builder.setPositiveButton("Handynummer") { dialog, _ -> dialog.cancel() }
-                builder.setNegativeButton("E-Mailadresse") { dialog, _ -> dialog.cancel() }
+                builder.setNegativeButton("E-Mail") { dialog, _ -> dialog.cancel() }
 
                 val alert = builder.create()
                 alert.show()
             }
         }
-
-        return rootView
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(CourseDetailViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
