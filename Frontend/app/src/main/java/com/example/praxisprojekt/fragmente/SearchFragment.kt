772777fa5e475e298e825763bcf4020a8f7266c7
KayRuck <kay.nicole.ruck@web.de>
2019-12-30 22:24:36 +0100

    delete on Avitiy Created

--- Frontend/app/src/main/java/com/example/praxisprojekt/fragmente/SearchFragment.kt
+++ Frontend/app/src/main/java/com/example/praxisprojekt/fragmente/SearchFragment.kt
@@ -4,35 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import com.example.praxisprojekt.R
-import com.example.praxisprojekt.viewModels.SearchViewModel
-import kotlinx.android.synthetic.main.search_fragment.view.*
 
 class SearchFragment : Fragment() {
-
-    private lateinit var viewModel: SearchViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.search_fragment, container, false)
-        val btn: Button = rootView.search_btn
-        btn.setOnClickListener {
-
-        }
-
-        return rootView
+        return inflater.inflate(R.layout.search_fragment, container, false)
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 
 }
