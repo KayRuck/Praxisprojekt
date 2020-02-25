8d756678103442ddd88ec327a33f6bb9603c5d8f
KayRuck <kay.ruck@yahoo.de>
2020-02-25 13:02:23 +0100

    Add functional Login Logic

--- Frontend/app/src/main/java/com/example/praxisprojekt/fragmente/LoginFragment.kt
+++ Frontend/app/src/main/java/com/example/praxisprojekt/fragmente/LoginFragment.kt
@@ -6,10 +6,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -24,15 +22,16 @@ class LoginFragment : Fragment() {
 
     private lateinit var email: EditText
     private lateinit var password: EditText
+
+    private lateinit var emailValid: TextView
+    private lateinit var passValid: TextView
+
     private lateinit var rootView: View
     private lateinit var signup: TextView
     private lateinit var forgot: TextView
     private lateinit var viewModel: LoginViewModel
-    private var retroEmail: String = ""
-    private var retroPass: String = ""
-    private var retroID: Int? = null
 
-    var functionTAG = " "
+    var functionTAG = "LoginFragment"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,32 +40,44 @@ class LoginFragment : Fragment() {
         rootView = inflater.inflate(R.layout.login_fragment, container, false)
         viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
 
+        Log.d(functionTAG, "Fragment loaded")
+
         //init viewModel observer
-        viewModel.oldUser.observe(this, Observer { setUserData(viewModel.retroOldUser) })
-        viewModel.newUser.observe(this, Observer { setUserData(viewModel.retroNewUser) })
+        viewModel.oldUser.observe(
+            this,
+            Observer { validateEMailInput(viewModel.retroOldUser) })
+        viewModel.newUser.observe(
+            this,
+            Observer { validateUserInput(viewModel.retroNewUser) })
 
 
         email = rootView.loginEMailTV
         password = rootView.loginPasswordTV
         forgot = rootView.loginForgot
         signup = rootView.loginRegister
+        emailValid = rootView.valemail
+        passValid = rootView.valpassword
 
 
-        val btnLogin = rootView.findViewById<Button>(R.id.loginButton)
+        val btnLogin = rootView.loginButton
         btnLogin.setOnClickListener {
+            Log.d(functionTAG, "SignUp Button clicked.")
+            val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+            val userId = sharedPref.getInt(MainActivity.USER_ID, -1)
 
-            if (loginDataCheck(email.text.toString(), password.text.toString())) {
-                val intent = Intent(activity, MainActivity::class.java)
-                startActivity(intent)
-            } else Toast.makeText(context, "Falsche Email oder Passwort", Toast.LENGTH_LONG).show()
+            if (userId != -1) viewModel.getUserData(userId)
+            else viewModel.checkEmail(email.text.toString())
 
         }
 
         signup.setOnClickListener {
+            Log.d(functionTAG, "SignUp Button clicked.")
+            val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+            val userId = sharedPref.getInt(MainActivity.USER_ID, -1)
+            Log.d(functionTAG, "USER_ID: $userId")
 
-            val fragTransaction1 = fragmentManager!!.beginTransaction()
-            fragTransaction1.replace(R.id.fragment_container_login, UserEditFragment()).commit()
-
+            if (userId != -1) viewModel.getUserData(userId)
+            else viewModel.checkEmail(email.text.toString())
         }
 
 
@@ -82,79 +93,66 @@ class LoginFragment : Fragment() {
         return rootView
     }
 
-    // TODO: Auslagern in das Viewmodel
-    private fun loginDataCheck(email: String, password: String): Boolean {
-        val checker: Boolean
-        functionTAG = "LOGIN DATA CHECK - "
+    private fun validateEMailInput(retroUser: RetroUser) {
 
-        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
-        val userId = sharedPref.getInt(MainActivity.USER_ID, -1)
-        if (userId != -1) {
-            viewModel.getUserData(userId)
-            checker = when {
-                email != retroEmail -> {
-                    Log.d(
-                        functionTAG + "E-Mail invalide",
-                        "Wrong E-Mail $email / $retroEmail for the saved shared pref User"
-                    )
-                    false
-                }
-                password != retroPass -> {
-                    Log.d(
-                        functionTAG + "Password invalide",
-                        "Wrong Password $password / $retroPass for the saved shared pref User"
-                    )
+        when {
+            retroUser.isNotValid() -> {
+                Log.d(
+                    functionTAG + "Not Such E-Mail",
+                    "There is no such E-Mail ${email.text} in the Database"
+                )
+                emailValid.setText("Keine passende E-Mail gefunden")
+                passValid.setText(" ")
 
-                    false
-                }
-                else -> true
             }
-            Log.d(
-                functionTAG + "E-Mail and Password valid",
-                "E-Mail and Password matches the saved shared pref User"
-            )
-        } else {
-            viewModel.checkEmail(email)
-            checker = when {
-                retroID == null -> {
-                    Log.d(
-                        functionTAG + "Not Such E-Mail",
-                        "There is no such E-Mail $retroEmail in the Database"
-                    )
-                    false
-                }
-                password != retroPass -> {
-                    Log.d(
-                        functionTAG + "Password invalide",
-                        "Wrong Password $retroPass for the saved shared pref User"
-                    )
-                    false
-                }
-                else -> {
-                    if (id != null) sharedPref.edit().putInt(MainActivity.USER_ID, id).apply()
-                    Log.d(
-                        functionTAG + "new ID in Shared Pref",
-                        "Set the new ID ($id) from the Main Database in shared Pref"
-                    )
-                    true
-                }
+            password.text.toString() != retroUser.password -> {
+                Log.d(
+                    functionTAG + "Password invalid",
+                    "Wrong Password ${password.text} for User with this E-Mail ${email.text}"
+                )
+                emailValid.setText("E-Mail vorhanden")
+                passValid.setText("Passwort ist falsch")
+
+            }
+            else -> {
+                val id: Int = retroUser.id ?: 1
+                val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+                sharedPref.edit().putInt(MainActivity.USER_ID, id).apply()
+                sharedPref.edit().putString(MainActivity.USER_NAME, retroUser.username).apply()
+
+                Log.d(
+                    functionTAG + "new ID in Shared Pref",
+                    "Set the new ID ($id) from the Main Database in shared Pref"
+                )
+                startMainActivity()
             }
         }
-
-        return checker
     }
 
-    private fun setUserData(retroUser: RetroUser) {
-        functionTAG = "SET EMAIL"
-
-        retroID = retroUser.id
-        retroEmail = retroUser.email
-        retroPass = retroUser.password
+    private fun validateUserInput(retroUser: RetroUser) {
+        when {
+            email.text.toString() != retroUser.email -> {
+                Log.d(
+                    functionTAG + "E-Mail invalid",
+                    "Wrong E-Mail ${email.text} / ${retroUser.email} for the saved shared pref User"
+                )
+            }
+            password.text.toString() != retroUser.password -> {
+                Log.d(
+                    functionTAG + "Password invalid",
+                    "Wrong Password ${password.text} / ${retroUser.password} for the saved shared pref User"
+                )
+            }
+            else -> startMainActivity()
+        }
         Log.d(
-            functionTAG, "Value of EMail Property $retroEmail \n" +
-                    "Value of Password Property $retroPass"
+            functionTAG + "E-Mail and Password valid",
+            "E-Mail and Password matches the saved shared pref User"
         )
     }
 
-
+    private fun startMainActivity() {
+        val intent = Intent(activity, MainActivity::class.java)
+        startActivity(intent)
+    }
 }
